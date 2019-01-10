8430d822b55919ca886971ac37774774788ce60e
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-01-10 17:19:13 +0800

    # 添加vue-awesome (图标库)

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,12 +7,14 @@ import store from './store'    //默认store目录下的index.js文件
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import './style/iviewTheme';
-
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon.vue';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(iView);
+Vue.component('v-icon', Icon);
 
 /* eslint-disable no-new */
 new Vue({
