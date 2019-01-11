313d774c3f638288ffa004e7cf8a8199a54fd8a1
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-01-11 11:15:01 +0800

    # 更新主界面布局

--- src/renderer/components/mainScreen/MainScreen.vue
+++ src/renderer/components/mainScreen/MainScreen.vue
@@ -4,25 +4,41 @@
 <template>
     <div class="layout">
         <Layout>
-            <Header :style="{position: 'fixed', width: '100%'}">
+            <Header class="header-layout" :style="{position: 'fixed', width: '100%'}">
                 <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+                    <div class="layout-logo">
+                        <Icon custom="i-icon icon-appicon" size="55"></Icon>
+                    </div>
                     <div class="layout-nav">
                         <MenuItem name="1">
-                            <Icon type="ios-navigate" size="45"></Icon>
+                            <Icon custom="i-icon icon-hardwareview" size="45"></Icon>
                             指标视图
                         </MenuItem>
                         <MenuItem name="2">
-                            <Icon type="ios-keypad" size="45"></Icon>
+                            <Icon custom="i-icon icon-warningview" size="45"></Icon>
                             告警视图
                         </MenuItem>
                         <MenuItem name="3">
-                            <Icon type="ios-analytics" size="45"></Icon>
+                            <Icon custom="i-icon icon-maintain-report" size="45"></Icon>
                             运维报表
                         </MenuItem>
                         <MenuItem name="4">
-                            <Icon type="ios-paper" size="45"></Icon>
+                            <Icon type="ios-star-outline" size="45"></Icon>
                             收藏夹
+                        </MenuItem>
+                    </div>
+                    <div class="layout-nav-right">
+                        <MenuItem name="1">
+                            <Icon type="md-settings"/>
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="md-remove"/>
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics" size="15"></Icon>
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="md-close"/>
                         </MenuItem>
                     </div>
                 </Menu>
