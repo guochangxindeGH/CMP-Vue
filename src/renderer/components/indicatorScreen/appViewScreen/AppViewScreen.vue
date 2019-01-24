3b2753df0dd87663c261aa688a81df4d168f8cd7
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-01-24 16:40:22 +0800

    # app中的card显示正常

--- src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
+++ src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
@@ -3,6 +3,25 @@
         <h2 style="background: orange">app视图</h2>
         <Button type="primary" @click="onClickForLogin">获取APP列表</Button>
         <p style="color: #321c67">{{hostList}}</p>
+        <Row>
+            <Col span="6" v-for="app in hostList">
+                <Card style="margin: 10px 20px">
+                    <p slot="title">
+                        {{app.AppName}}
+                    </p>
+                    <ul>
+                        <li v-for="item in 4">
+                            <a :href="item.url" target="_blank">name</a>
+                            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half"
+                                                                                                                                    v-else></Icon>
+rate
+                </span>
+                        </li>
+                    </ul>
+                </Card>
+            </Col>
+        </Row>
     </div>
 </template>
 <script>
@@ -18,15 +37,18 @@
         data() {
             return {
                 appList: [],
-                hostList: 111
+                hostList: dataManager.getAppList()
             };
+        },
+        mounted: function () {
+            console.log('AppView Mount');
         },
         methods: {
             onClickForLogin() {
                 this.$http
                     .get('http://172.24.119.2:9090/api/v1/query?query=up')
                     .then((response) => {
-                        this.hostList = response.data.data.result;
+                        // this.hostList = response.data.data.result;
                         console.log('主机个数:' + this.hostList.length);
                         console.log('this.instance' + this.hostList[0].metric.instance);
                         console.log('this.up:' + this.hostList[0].value[1]);
