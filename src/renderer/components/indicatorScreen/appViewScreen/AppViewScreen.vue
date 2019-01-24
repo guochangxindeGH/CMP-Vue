e33847e8c5ac2aec8831cb00ede4f5b587d24d61
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-01-24 17:08:47 +0800

    # CPU数据更新成功

--- src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
+++ src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
@@ -1,22 +1,15 @@
 <template>
     <div>
-        <h2 style="background: orange">app视图</h2>
-        <Button type="primary" @click="onClickForLogin">获取APP列表</Button>
-        <p style="color: #321c67">{{hostList}}</p>
         <Row>
             <Col span="6" v-for="app in hostList">
                 <Card style="margin: 10px 20px">
-                    <p slot="title">
+                    <p slot="title" style="text-align: center">
                         {{app.AppName}}
                     </p>
                     <ul>
-                        <li v-for="item in 4">
-                            <a :href="item.url" target="_blank">name</a>
-                            <span>
-                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half"
-                                                                                                                                    v-else></Icon>
-rate
-                </span>
+                        <li style="margin-top: 10px" v-for="(attrValue, attrName) of app.attrList">
+                            <span style="color: black">{{attrName}}:</span>
+                            <span style="color: black">{{attrValue}}</span>
                         </li>
                     </ul>
                 </Card>
@@ -26,6 +19,7 @@ rate
 </template>
 <script>
     import * as dataManager from 'dirManagers/dataManager';
+    import Vue from 'vue';
 
     export default {
         created: function () {
@@ -36,14 +30,93 @@ rate
         },
         data() {
             return {
-                appList: [],
-                hostList: dataManager.getAppList()
+                timer: null,
+                baseUrl: 'http://172.24.119.2:9090/api/v1/query?',
+                hostList: null
             };
         },
         mounted: function () {
-            console.log('AppView Mount');
+            this.hostList = dataManager.getAppList();
+            console.log('App 挂载，host list:' + this.hostList);
+            // 添加指标
+            for (let host of this.hostList) {
+                host.attrList = {
+                    '地址': host.Address,
+                    'Active': '----',
+                    'CpuUsage': '----',
+                    'MemUsage': '----',
+                    'DiskUsage': '----'
+                };
+            }
+            this.timer = setInterval(this.getAttrValues, 2000);
+        },
+        beforeDestroy: function () {
+            if (this.timer) {
+                console.log('停掉定时器');
+                clearInterval(this.timer);
+            }
         },
         methods: {
+            getAttrValues() {
+                console.log('更新指标');
+                this.$http
+                    .get(this.baseUrl + 'query=up')
+                    .then(this.checkResult)
+                    .then(this.updateActive);
+                this.$http
+                    .get(this.baseUrl + 'query=rate(node_cpu_seconds_total{cpu="0",mode="idle"}[5m])')
+                    .then(this.checkResult)
+                    .then(this.updateCpuUsage);
+            },
+            checkResult(response) {
+                if (response.status === 200) {
+                    let resultList = [];
+                    for (let host of response.data.data.result) {
+                        let ip = host.metric.instance.split(':')[0];
+                        let value = host.value[1];
+                        resultList.push({
+                            ip: ip,
+                            value: value
+                        });
+                    }
+                    return resultList;
+                }
+                return null;
+            },
+            updateActive(activeList) {
+                if (activeList === null) {
+                    console.log('指标为空');
+                    return;
+                }
+                for (let hostItem of activeList) {
+                    for (let index in this.hostList) {
+                        let hostInSys = this.hostList[index];
+                        if (hostInSys.Address === hostItem.ip) {
+                            if (hostItem.value === '1') {
+                                hostInSys.attrList.Active = '活跃';
+                            } else {
+                                hostInSys.attrList.Active = '不活跃';
+                            }
+                            Vue.set(this.hostList, index, hostInSys);
+                        }
+                    }
+                }
+            },
+            updateCpuUsage(cpuList) {
+                if (cpuList === null) {
+                    console.log('指标为空');
+                    return;
+                }
+                for (let hostItem of cpuList) {
+                    for (let index in this.hostList) {
+                        let hostInSys = this.hostList[index];
+                        if (hostInSys.Address === hostItem.ip) {
+                            hostInSys.attrList.CpuUsage = (hostItem.value * 100).toFixed(1) + '%';
+                            Vue.set(this.hostList, index, hostInSys);
+                        }
+                    }
+                }
+            },
             onClickForLogin() {
                 this.$http
                     .get('http://172.24.119.2:9090/api/v1/query?query=up')
