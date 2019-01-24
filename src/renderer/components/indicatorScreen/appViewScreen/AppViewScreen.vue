705f0e5f1a7103ffdc74dd8630de6eb9cbf1ae36
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-01-24 16:15:38 +0800

    # 调试Prometheus格式数据

--- src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
+++ src/renderer/components/indicatorScreen/appViewScreen/AppViewScreen.vue
@@ -2,24 +2,7 @@
     <div>
         <h2 style="background: orange">app视图</h2>
         <Button type="primary" @click="onClickForLogin">获取APP列表</Button>
-        <div v-for="app in appList">
-            <Card style="width:350px">
-                <p slot="app.Address">
-                    <Icon type="ios-film-outline"></Icon>
-                    Classic film
-                </p>
-                <ul>
-                    <li v-for="item in randomMovieList">
-                        <a :href="item.url" target="_blank">{{ item.name }}</a>
-                        <span>
-                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half"
-                                                                                                                                    v-else></Icon>
-                    {{ item.rate }}
-                </span>
-                    </li>
-                </ul>
-            </Card>
-        </div>
+        <p style="color: #321c67">{{hostList}}</p>
     </div>
 </template>
 <script>
@@ -34,13 +17,20 @@
         },
         data() {
             return {
-                appList: []
+                appList: [],
+                hostList: 111
             };
         },
         methods: {
             onClickForLogin() {
-                this.appList = dataManager.getAppList();
-                debugger;
+                this.$http
+                    .get('http://172.24.119.2:9090/api/v1/query?query=up')
+                    .then((response) => {
+                        this.hostList = response.data.data.result;
+                        console.log('主机个数:' + this.hostList.length);
+                        console.log('this.instance' + this.hostList[0].metric.instance);
+                        console.log('this.up:' + this.hostList[0].value[1]);
+                    });
             }
         }
     };
